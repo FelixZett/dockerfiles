b0fd41371876e559753421b9c20f54df521c08ac
FelixZett <FelixZett@users.noreply.github.com>
2020-02-06 08:16:54 +0100

    Add image for hugo-extended

---
+++ hugo-extended/howtobuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "INFO: Building felixzett/hugo-extended..."
+echo "INFO: Looking for newest Hugo version..."
+VERSION=$(curl --silent "https://api.github.com/repos/gohugoio/hugo/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+VERSION=${VERSION:1}
+echo "INFO: Latest version is $VERSION"
+echo "INFO: Building image..."
+docker build --build-arg VERSION=$VERSION -t felixzett/hugo-extended:$VERSION .
+
+docker push felixzett/hugo-extended:$VERSION
+
+docker tag felixzett/hugo-extended:$VERSION felixzett/hugo-extended:latest
+
+docker push felixzett/hugo-extended:latest
+
+echo "INFO: felixzett/hugo-extended DONE"
+
